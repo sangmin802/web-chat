472650a24519028897950a5bff74adbf4f9436fb
DESKTOP-LB3HQR1\USER <sangmin802@naver.com>
2021-05-17 17:49:48 +0900

    Chat 컴포넌트 입력창 및, 메시지 노출 창

---
+++ src/components/chat/index.tsx
@@ -0,0 +1,39 @@
+import React, { useCallback, useRef } from "react";
+import { IChat } from "types/chat";
+import Message from "components/message/index";
+
+interface Props {
+  chats: IChat[];
+  sendPublicMessage(T: string): void;
+}
+
+const Chat = ({ chats, sendPublicMessage }: Props) => {
+  const ref = useRef<HTMLInputElement>(null);
+  const onSubmitHandler = useCallback(
+    e => {
+      e.preventDefault();
+      const message = ref?.current?.value;
+      if (message) {
+        sendPublicMessage(message);
+        e.target[0].value = "";
+      }
+    },
+    [sendPublicMessage, ref]
+  );
+
+  return (
+    <section>
+      <ul className="chats">
+        {chats.map((chat, i) => (
+          <Message key={`public chat ${i}`} {...chat} />
+        ))}
+      </ul>
+      <form onSubmit={onSubmitHandler}>
+        <input ref={ref} type="text" />
+        <button>입력</button>
+      </form>
+    </section>
+  );
+};
+
+export default Chat;
