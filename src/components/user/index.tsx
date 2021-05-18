32fc6b2c657caac9295f3244e099d3475c475d1a
DESKTOP-LB3HQR1\USER <sangmin802@naver.com>
2021-05-18 16:51:17 +0900

    users 귓속말을 위해 선택한 유저 toggle버튼

--- src/components/user/index.tsx
+++ src/components/user/index.tsx
@@ -3,9 +3,16 @@ import { IUser } from "types/user";
 
 interface Props {
   user: IUser;
+  setSelectedUser(T: null | IUser): void;
+  selectedUser: null | IUser;
 }
 
-const User = ({ user }: Props) => {
+const User = ({ user, setSelectedUser, selectedUser }: Props) => {
+  const onClickHandler = useCallback(() => {
+    const toggle = selectedUser?.userID === user.userID ? null : user;
+    setSelectedUser(toggle);
+  }, [setSelectedUser, user, selectedUser]);
+
   return (
     <article>
       <div className="user-name">{user.userName}</div>
