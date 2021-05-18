6d0e4a515a58f5fed9eb0b21d5f97b4f014ad49c
DESKTOP-LB3HQR1\USER <sangmin802@naver.com>
2021-05-18 16:53:06 +0900

    use socket 입장 알림 추가 퇴장 알림 추가   퇴장 시, 선택된 유저였다면 알림 문구 변경 및 선택유저 초기화

--- src/hooks/use-socket.ts
+++ src/hooks/use-socket.ts
@@ -28,23 +28,44 @@ export function useSocket({
 
     socket.on("user connected", user => {
       setUser(user);
+      setChat({ content: `${user.userName}님이 입장하셨습니다.` });
     });
 
-    socket.on("user disconnected", userID => {
+    socket.on("user disconnected", ({ userID, userName }) => {
+      const isQuit = selectedUser?.userID === userID ? true : false;
+      let content = `${userName}님이 퇴장하셨습니다.`;
+      if (isQuit) {
+        content = `귓속말 대상인 ${userName}님이 퇴장하셨습니다.`;
+        setSelectedUser(null);
+      }
+      setChat({ content });
       removeUser(userID);
     });
 
-    socket.on("send public message", message => {
-      setChat(message);
+    const messageEvents = ["public message", "private message"];
+    messageEvents.forEach(event => {
+      socket.on(event, message => {
+        const fromSelf = message.from.userID === socket.userID ? true : false;
+        setChat({ ...message, fromSelf });
+      });
     });
 
     return () => {
       socket.off("users");
       socket.off("user connected");
       socket.off("user disconnected");
-      socket.off("send public message");
+      socket.off("public message");
+      socket.off("private message");
     };
-  }, [isLogin, setUsers, setUser, removeUser, setChat]);
+  }, [
+    isLogin,
+    setUsers,
+    setUser,
+    removeUser,
+    setChat,
+    selectedUser,
+    setSelectedUser,
+  ]);
 
   useEffect(() => {
     socket.on("session", userID => {
