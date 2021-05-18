584454611e99a2a85cd0bc162cfcd2a1478f91c8
DESKTOP-LB3HQR1\USER <sangmin802@naver.com>
2021-05-18 16:55:49 +0900

    app use select user훅 private message 이벤트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { useLogin } from "hooks/use-login";
 import { useSocket } from "hooks/use-socket";
 import { useUsers } from "hooks/use-users";
 import { useChat } from "hooks/use-chat";
+import { useSelectUser } from "hooks/use-select-user";
 import Login from "components/login/index";
 import Loby from "components/loby";
 
@@ -10,13 +11,16 @@ function App() {
   const { isLogin, setLogin } = useLogin();
   const { users, setUsers, setUser, removeUser } = useUsers();
   const { chats, setChat, clearChat } = useChat();
-  const { connectSocekt, sendPublicMessage } = useSocket({
+  const { selectedUser, setSelectedUser } = useSelectUser();
+  const { connectSocekt, sendPublicMessage, sendPrivateMessage } = useSocket({
     isLogin,
     setLogin,
     setUsers,
     setUser,
     removeUser,
     setChat,
+    selectedUser,
+    setSelectedUser,
   });
   return (
     <div className="app">
@@ -27,6 +31,9 @@ function App() {
             users={users}
             chats={chats}
             sendPublicMessage={sendPublicMessage}
+            sendPrivateMessage={sendPrivateMessage}
+            selectedUser={selectedUser}
+            setSelectedUser={setSelectedUser}
           />
         )}
       </main>
