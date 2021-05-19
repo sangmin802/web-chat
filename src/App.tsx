ef36f61a04aeff972d9bd937772608ede435d14c
DESKTOP-LB3HQR1\USER <sangmin802@naver.com>
2021-05-19 18:29:52 +0900

    app 1. 새로운 hook 및 room-loby 컴포넌트 활성화 조건 생성

--- src/App.tsx
+++ src/App.tsx
@@ -3,29 +3,39 @@ import { useLogin } from "hooks/use-login";
 import { useSocket } from "hooks/use-socket";
 import { useUsers } from "hooks/use-users";
 import { useChat } from "hooks/use-chat";
+import { useRoom } from "hooks/use-room";
+import { useRooms } from "hooks/use-rooms";
 import { useSelectUser } from "hooks/use-select-user";
 import Login from "components/login/index";
 import Loby from "components/loby";
+import RoomLoby from "components/room-loby";
 
 function App() {
   const { isLogin, setLogin } = useLogin();
-  const { users, setUsers, setUser, removeUser } = useUsers();
-  const { chats, setChat, clearChat } = useChat();
+  const { users, setUsers } = useUsers();
+  const { room, setRoom } = useRoom();
+  const { rooms, setRooms } = useRooms();
+  const { chats, setChat } = useChat(room);
   const { selectedUser, setSelectedUser } = useSelectUser();
   const { connectSocekt, sendPublicMessage, sendPrivateMessage } = useSocket({
     isLogin,
     setLogin,
+    users,
     setUsers,
-    setUser,
-    removeUser,
     setChat,
     selectedUser,
     setSelectedUser,
+    rooms,
+    setRooms,
   });
+
   return (
     <div className="app">
       <main>
         {!isLogin && <Login connectSocekt={connectSocekt} />}
+        {isLogin && room && (
+          <RoomLoby room={room} setRoom={setRoom} chats={chats} />
+        )}
         {isLogin && users && (
           <Loby
             users={users}
@@ -34,6 +44,7 @@ function App() {
             sendPrivateMessage={sendPrivateMessage}
             selectedUser={selectedUser}
             setSelectedUser={setSelectedUser}
+            rooms={rooms}
           />
         )}
       </main>
