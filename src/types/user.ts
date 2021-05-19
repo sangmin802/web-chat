031373b9f72353db32ec0787933c5d11806a2358
DESKTOP-LB3HQR1\USER <sangmin802@naver.com>
2021-05-19 18:25:50 +0900

    types / user 새로운 메시지 수량 및, 최신 메시지 날짜 속성 추가

--- src/types/user.ts
+++ src/types/user.ts
@@ -1,4 +1,14 @@
+export interface IUserID {
+  userName: string;
+  userID: string;
+}
+
 export interface IUser {
   userName: string;
   userID: string;
+  self: boolean;
+  messages: {
+    size: number;
+    recent: Date;
+  };
 }
