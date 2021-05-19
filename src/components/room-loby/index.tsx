f1ef37cc11118714888db07eeeb31c9676daeefc
DESKTOP-LB3HQR1\USER <sangmin802@naver.com>
2021-05-19 18:20:43 +0900

    components / room-loby 현재 활성화중인 room view

---
+++ src/components/room-loby/index.tsx
@@ -0,0 +1,43 @@
+import React, { useCallback, useMemo } from "react";
+import { IChat } from "types/chat";
+import { IRoom } from "types/room";
+import Chat from "components/chat";
+
+interface Props {
+  room: IRoom;
+  setRoom(T: null): void;
+  chats: IChat[];
+}
+
+const RoomLoby = ({ room, setRoom, chats }: Props) => {
+  const emitMessage = useCallback(message => {}, []);
+
+  const goLobbyHandler = useCallback(() => {
+    setRoom(null);
+  }, [setRoom]);
+
+  const leaveRoomHandler = useCallback(() => {}, [room]);
+
+  const users = useMemo(() => {
+    return [...room.users.values()];
+  }, [room.users]);
+
+  return (
+    <section className="message-room">
+      <div className="leave-room" onClick={leaveRoomHandler}>
+        나가기
+      </div>
+      <div className="go-lobby" onClick={goLobbyHandler}>
+        로비로 이동
+      </div>
+      <div className="room-users">
+        {users.map(user => (
+          <>{user.userName}</>
+        ))}
+      </div>
+      <Chat chats={chats} emitMessage={emitMessage} />
+    </section>
+  );
+};
+
+export default RoomLoby;
