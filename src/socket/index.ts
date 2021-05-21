f42fa8c477d6a2646a918a9f7d58a40140e7ec4f
DESKTOP-LB3HQR1\USER <sangmin802@naver.com>
2021-05-21 20:00:28 +0900

    socket 모든 이벤트 관련 console 지저분하여 일단 끔

--- src/socket/index.ts
+++ src/socket/index.ts
@@ -8,5 +8,5 @@ const url = "http://localhost:3001/web-chat";
 export const socket: ISocet = io(url, { autoConnect: false });
 
 socket.onAny((event, ...args) => {
-  console.log(event, args);
+  // console.log(event, args);
 });
