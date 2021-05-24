02412b70ddb2065a7823e0b4a69be690760b99f9
DESKTOP-LB3HQR1\USER <sangmin802@naver.com>
2021-05-24 17:05:39 +0900

    components/room 입장한 방의 경우 새로운 메시지와, 현재 참여중인 유저리스트 노출

--- src/components/room/index.tsx
+++ src/components/room/index.tsx
@@ -13,7 +13,29 @@ const Room = ({ room, setRoom, joinRoom }: Props) => {
     joinRoom(room.roomID);
   }, [setRoom, joinRoom, room]);
 
-  return <article onClick={joinRoomHandler}>{room.roomName}</article>;
+  const [primaryUser, size] = useMemo(() => {
+    if (!room.isJoined) return [null, null];
+    const primary = room.users[0].userName;
+    const size = room.users.length < 2 ? null : room.users.length - 1;
+    return [primary, size];
+  }, [room.users, room.isJoined]);
+
+  return (
+    <SRoom isJoined={room.isJoined} onClick={joinRoomHandler}>
+      <div className="room-name">{room.roomName}</div>
+      {room.isJoined && (
+        <>
+          <div className="room-users">
+            {primaryUser} {size && `+${size}`}
+          </div>
+          {room.hasNewMessages !== 0 && (
+            <div className="room-hasNewMessages">{room.hasNewMessages}</div>
+          )}
+        </>
+      )}
+      {!room.isJoined && <div>대화방에 참여하세요!</div>}
+    </SRoom>
+  );
 };
 
 export default Room;
