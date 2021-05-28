217b4cbe02fd90b67c287a89fc50c42d7dc84ec9
DESKTOP-LB3HQR1\USER <sangmin802@naver.com>
2021-05-28 17:25:42 +0900

    user 스타일 지정 css-in-js

--- src/components/user/index.tsx
+++ src/components/user/index.tsx
@@ -29,8 +29,21 @@ const User = ({ user, togglePrivateMessage, selectedUser }: Props) => {
 };
 
 const SArticle = styled.article<{ isTarget: boolean }>`
-  background-color: ${({ isTarget }) =>
-    isTarget ? "yellowgreen" : "transparent"};
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-item: center;
+  ${({ isTarget }) =>
+    isTarget
+      ? "background-color : #fff; color : black"
+      : "background-color : transparent; color : #f2f2f2"};
+  padding: 0.2rem 0.4rem;
+  .messages-hasNewMessages {
+    background-color: purple;
+    padding: 0.1rem 0.3rem;
+    border-radius: 3px;
+    font-size: 0.85rem;
+  }
 `;
 
 export default User;
