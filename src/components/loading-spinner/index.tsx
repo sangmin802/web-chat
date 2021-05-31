8d68aa0e3af6e4afb887b65604a01dc057911ad0
DESKTOP-LB3HQR1\USER <sangmin802@naver.com>
2021-05-31 13:04:24 +0900

    loading-spinner 컴포넌트 추가

---
+++ src/components/loading-spinner/index.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "./index.css";
+
+const LoadingSpinner = () => {
+  return (
+    <div className="lds-spinner">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
+
+export default LoadingSpinner;
